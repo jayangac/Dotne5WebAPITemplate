c37e16830ee9334933e864f7253088fb600697aa
WINDOWS-PRI5HCL\Jayanga <jayanga.chandrakumara@qualitapps.com>
2021-08-21 14:06:50 +0530

    Adding login DTO

---
+++ Dotne5WebAPITemplate.Models/DTO/LoginDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dotne5WebAPITemplate.Models.DTO
+{
+    public class LoginDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
